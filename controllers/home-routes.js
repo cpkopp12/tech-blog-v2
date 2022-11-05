104bc6a584349c045db9d5db6591312062561a46
cpkopp <cameronpkopp@gmail.com>
2022-11-05 11:21:27 -1000

    singlepost handelbar file renders, add front end js next

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 //DECLARATIONS: router, db connection, model object -------------
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User, Post, Comment } =require('../models');
+const { User, Post, Comment } = require('../models');
 
 //HOMEROUTES ==========================
 router.get('/', (req, res) => {
@@ -41,7 +41,42 @@ router.get('/login', (req, res) => {
         return;
     }
     res.render('login');
-})
+});
+
+//single post page: /post/:id
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: { id: req.params.id },
+        attributes: ['id','post_text','title','created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id','comment_text','post_id','user_id','created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbData => {
+            if (!dbData) {
+                res.status(404).json({ message: 'No post found with this id'});
+            }
+            //unpack dbData
+            const post = dbData.get({ plain: true });
+
+            res.render('single-post', { post });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 //EXPORT ROUTER ------
 module.exports = router;
