b189c954bb0177347e3cc76d69d3c4a638dd6b70
cpkopp <cameronpkopp@gmail.com>
2022-11-05 00:34:33 -1000

    homepage all set, will need to work on styling in main.handlebars, next login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,36 @@
-//DECLARATIONS: router -------------
+//DECLARATIONS: router, db connection, model object -------------
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Post, Comment } =require('../models');
 
 //HOMEROUTES ==========================
 router.get('/', (req, res) => {
-    res.render('homepage', {
-        id: 1,
-        post_text: "i is queen and blogger",
-        title: "test post",
-        created_at: new Date(),
-        comments: [{},{}],
-        user: {
-            username: 'user1'
-        }
-    });
+    Post.findAll({
+        attributes: ['id', 'post_text', 'title', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbData => {
+            //unpack dbData object
+            const posts = dbData.map(post => post.get({ plain: true }))
+            res.render('homepage', { posts });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
 });
 
 //EXPORT ROUTER ------
