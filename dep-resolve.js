3e507466d9a7f764bc0b86e69d0665641e6cac76
cpkopp <cameronpkopp@gmail.com>
2022-11-06 23:54:22 -1000

    not

--- dep-resolve.js
+++
@@ -1,8 +0,0 @@
-const { resolver } = require('resolve-package-json');
-const pkg = require('./package-lock.json');
- 
-resolver(pkg.dependencies, function (err, result) {
-  if (err) throw err;
- 
-  console.log(result);
-});
